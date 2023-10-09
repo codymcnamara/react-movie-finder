e9331945ac77470b6f39057189eba4bc39d1c65f
codymcnamara <codyjamesmcnamara@gmail.com>
2023-10-09 16:20:27 -0700

    set up react router for home and about

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h2>Movie Finder Home page</h2>
+  },
+  {
+    path: '/about',
+    element: (
+      <div><h2>Movie Finder About Page</h2></div>
+    )
+  }
+]);
 function App() {
   return (
-    <h2>Movie Finder</h2>
+    <RouterProvider router={router}/>
   )
 }
 
