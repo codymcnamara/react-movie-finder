4967b02a6beadadf32fca0ed33d2ca4c64be6671
codymcnamara <codyjamesmcnamara@gmail.com>
2023-10-12 19:32:37 -0700

    add nav bar and style it with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './bootstrap.scss';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
   HomeLayout,
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,43 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const NavBar = ()=> {
+    const [isOpen, setOpen] = useState(false);
+
+    const handleClick = () : void => {
+        setOpen(!isOpen);
+    }
+
     return (
-        <nav>
-            <h1>Movie Finder</h1>
-            <NavLink to='/' className='nav-link'>
-                Home
-            </NavLink>
-            <NavLink to='/about' className='nav-link'>
-                About
-            </NavLink>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <NavLink to='/' className='navbar-brand'>
+                    Movie Finder
+                </NavLink>
+                <button 
+                    onClick={ handleClick }
+                    className="navbar-toggler" 
+                    type="button" 
+                    aria-controls="navbarNavAltMarkup" 
+                    aria-expanded={ isOpen ? 'false' : 'true' }
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div 
+                    className={`collapse navbar-collapse${isOpen ? ' show' : ''}`} 
+                    id="navbarNavAltMarkup"
+                >
+                    <div className="navbar-nav">
+                        <NavLink to='/' className='nav-link'  >
+                            Home
+                        </NavLink>
+                        <NavLink to='/about' className='nav-link'>
+                            About
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
