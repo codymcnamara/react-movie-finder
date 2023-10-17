c9c6b074cf08e4bdc92084d40949caada22434a5
codymcnamara <codyjamesmcnamara@gmail.com>
2023-10-16 17:56:02 -0700

    movie cards: add fallback images and ensure same aspect ratio

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -6,7 +6,7 @@ export const loader = async ()=> {
     let initialQuery = 'https://api.themoviedb.org/3/search/movie?query=zoolander&api_key=7b148caa8720b72c9e6ddf7882939bdc'
     const response = await axios.get(initialQuery);
     const data = response.data;
-    // console.log(data);
+    console.log(data);
     return data.results;
 }
 
@@ -15,10 +15,12 @@ const MovieList = () => {
     const baseUrl = 'https://image.tmdb.org/t/p/w500/';
 
     const movieDivs: JSX.Element[] = movieData.map((movie: Movie)=>{
+        let imgPath: string = movie.backdrop_path ? movie.backdrop_path : movie.poster_path;
+
         return (
-            <div className="col">
-                <div className="card" key={movie.id}>
-                    <img src={baseUrl + movie.backdrop_path}className="card-img-top" alt="..."/>
+            <div className="col" key={movie.id}>
+                <div className="card" >
+                    <img src={baseUrl + imgPath}className="card-img-top wide-aspect-ratio" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{movie.title}</h5>
                         <p className="card-text">{movie.overview}</p>
