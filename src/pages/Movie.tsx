5d8878c4cab2c0420acfe0d1d600fc2b1a1c6e1c
codymcnamara <codyjamesmcnamara@gmail.com>
2023-10-20 12:44:39 -0700

    add more details to movie detail page

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -6,6 +6,18 @@ const Movie = ()=> {
     const movie = useLoaderData() as MovieDetail;
     const baseUrl = 'https://image.tmdb.org/t/p/original';
     const imgPath: string = movie.poster_path ? movie.poster_path : movie.backdrop_path;
+    const listItemData: object[] = [
+        {subHeading: 'Tagline', content: movie.tagline},
+        {subHeading: 'Genres', content: movie.genres.map( (genre) => genre.name ).join(', ')},
+        {subHeading: 'Overview', content: movie.overview},
+        {subHeading: 'Release Date', content: movie.release_date},
+        {subHeading: 'Budget', content: `$${movie.budget.toLocaleString()}`},
+        {subHeading: 'Revenue', content: `$${movie.revenue.toLocaleString()}`},
+        {subHeading: 'Runtime', content: `${movie.runtime} minutes`},
+        {subHeading: 'Vote Average', content: movie.vote_average},
+        {subHeading: 'Vote Count', content: movie.vote_count.toLocaleString()},
+        {subHeading: 'Popularity', content: movie.popularity},
+    ];
 
     return (
         <>
@@ -16,9 +28,9 @@ const Movie = ()=> {
                 </div>
                 <div className="col">
                     <ul className="list-group">
-                        <MovieListItem {...{subHeading: 'Tagline', content: movie.tagline}}/>
-                        <MovieListItem {...{subHeading: 'Genres', content: movie.genres.map( (genre) => genre.name ).join(', ')}}/>
-                        <MovieListItem {...{subHeading: 'Overview', content: movie.overview}}/>
+                        {listItemData.map((item)=>{
+                            return <MovieListItem {...item}/>
+                        })}
                     </ul> 
                 </div>
             </div>
