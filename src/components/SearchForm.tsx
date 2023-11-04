6f4da4dab7ffca8db0adb72fa9b9f7b7dd088e60
codymcnamara <codyjamesmcnamara@gmail.com>
2023-11-03 18:01:46 -0700

    CSS styling for search form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,10 +2,13 @@ import { Form } from "react-router-dom";
 
 const SearchForm = () => {
     return (
-        <Form action="">
-            <input type="text" name="search" />
-            <button type="submit" >Search</button>
-        </Form>
+        <div className="form-wrapper">
+            <Form action="" className="mb-5 search-form">
+                <label for="search" class="form-label">Search for your favorite movie</label>
+                <input type="text" name="search" id="search" className="form-control mb-3" />
+                <button type="submit" className="btn btn-primary mb-3 " >Search</button>
+            </Form>
+        </div>
     )
 }
 
